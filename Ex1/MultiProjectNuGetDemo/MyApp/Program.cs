3c0a21f3d1eb0c96168ce31a59d7dc6b1fce36e7
Piotrus00 <117126497+Piotrus00@users.noreply.github.com>
2025-03-09 16:28:45 +0100

    Lab1
    Lab1

---
+++ Ex1/MultiProjectNuGetDemo/MyApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using MyLibrary;
+using Newtonsoft.Json;
+using Microsoft.Extensions.DependencyInjection;
+using MyServices;
+
+public class MyApp
+{
+    static void Main(string[] args)
+    {
+        Calculator obj = new Calculator();
+        // Console.WriteLine(obj.Add(1,2));
+        // Console.WriteLine(obj.Subtract(1,2));
+        
+        double sum = obj.Add(5, 3);
+        var result = new { Operation = "Add", A = 5, B = 3, Result = sum };
+        string jsonResult = JsonConvert.SerializeObject(result, Formatting.Indented);
+        Console.WriteLine(jsonResult);
+        
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<ILoggerService, ConsoleLogger>()
+            .BuildServiceProvider();
+
+// Uzyskanie instancji loggera
+        var logger = serviceProvider.GetService<ILoggerService>();
+        logger.Log("Aplikacja uruchomiona.");
+
+// Przykładowe użycie kalkulatora
+        double suma = obj.Add(10, 15);
+        logger.Log($"Wynik dodawania: {sum}");
+
+    }
+}
---
+++ Ex1/MultiProjectNuGetDemo/MyLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace MyLibrary;
+
+public class Calculator
+{
+    public double Add(int a, int b)
+    {
+        return (a + b);
+    }
+
+    public double Subtract(int a, int b)
+    {
+        return (a - b);
+    }
+}
+
