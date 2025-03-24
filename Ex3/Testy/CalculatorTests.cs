d5eb1917e608cbac3cee979b1ba508e09f9eea13
Piotrus00 <117126497+Piotrus00@users.noreply.github.com>
2025-03-24 23:02:36 +0100

    Add files via upload

---
+++ Ex3/Testy/CalculatorTests.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using Kalkulator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KalkulatorTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_ReturnsCorrectSum()
+        {
+            Assert.AreEqual(5, Calculator.Add(2, 3));
+            Assert.AreEqual(-1, Calculator.Add(2, -3));
+            Assert.AreEqual(0, Calculator.Add(0, 0));
+        }
+
+        [Test]
+        public void Substract_ReturnsCorrectDifference()
+        {
+            Assert.AreEqual(2, Calculator.Substract(5, 3));
+            Assert.AreEqual(-5, Calculator.Substract(-2, 3));
+            Assert.AreEqual(0, Calculator.Substract(4, 4));
+        }
+
+        [Test]
+        public void Multiply_ReturnsCorrectProduct()
+        {
+            Assert.AreEqual(6, Calculator.Multiply(2, 3));
+            Assert.AreEqual(-6, Calculator.Multiply(-2, 3));
+            Assert.AreEqual(0, Calculator.Multiply(0, 5));
+        }
+
+        [Test]
+        public void Divide_ReturnsCorrectQuotient()
+        {
+            Assert.AreEqual(2, Calculator.Divide(6, 3));
+            Assert.AreEqual(-2, Calculator.Divide(-6, 3));
+            Assert.AreEqual(0.5, Calculator.Divide(1, 2), 0.0001);
+        }
+
+        [Test]
+        public void Divide_ByZero_ThrowsException()
+        {
+            Assert.Throws<DivideByZeroException>(() => Calculator.Divide(5, 0));
+        }
+
+        [Test]
+        public void SumSequence_ReturnsCorrectSum()
+        {
+            Assert.AreEqual(10, Calculator.SumSequence(new List<double> { 2, 3, 5 }));
+            Assert.AreEqual(0, Calculator.SumSequence(new List<double> { }));
+            Assert.AreEqual(-6, Calculator.SumSequence(new List<double> { -2, -4 }));
+        }
+
+        [Test]
+        public void AverageSequence_ReturnsCorrectAverage()
+        {
+            Assert.AreEqual(3, Calculator.AverageSequence(new List<double> { 2, 3, 4 }));
+            Assert.AreEqual(-3, Calculator.AverageSequence(new List<double> { -2, -3, -4 }));
+        }
+
+        [Test]
+        public void AverageSequence_EmptyList_ThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() => Calculator.AverageSequence(new List<double> { }));
+        }
+
+        [Test]
+        public void MaxSequence_ReturnsMaxValue()
+        {
+            Assert.AreEqual(5, Calculator.MaxSequence(new List<double> { 1, 2, 5, 3 }));
+            Assert.AreEqual(-1, Calculator.MaxSequence(new List<double> { -5, -3, -1 }));
+        }
+
+        [Test]
+        public void MinSequence_ReturnsMinValue()
+        {
+            Assert.AreEqual(1, Calculator.MinSequence(new List<double> { 1, 2, 5, 3 }));
+            Assert.AreEqual(-5, Calculator.MinSequence(new List<double> { -5, -3, -1 }));
+        }
+    }
+}
---
+++ Ex3/Testy/ScientificCalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Kalkulator;
+using System;
+
+namespace Testy
+{
+    [TestFixture]
+    public class ScientificCalculatorTests
+    {
+        [Test]
+        public void Power_ValidInputs_ReturnsCorrectResult()
+        {
+            Assert.AreEqual(8, ScientificCalculator.Power(2, 3));  // 2^3 = 8
+            Assert.AreEqual(1, ScientificCalculator.Power(5, 0));  // 5^0 = 1
+            Assert.AreEqual(2, ScientificCalculator.Power(4, 0.5)); // sqrt(4) = 2
+            Assert.AreEqual(0.1, ScientificCalculator.Power(10, -1), 0.0001); // 10^-1 = 0.1
+        }
+
+        [Test]
+        public void SquareRoot_ValidAndInvalidInputs()
+        {
+            Assert.AreEqual(3, ScientificCalculator.SquareRoot(9));  // sqrt(9) = 3
+            Assert.AreEqual(-1, ScientificCalculator.SquareRoot(0)); // sqrt(0) → -1
+            Assert.AreEqual(-1, ScientificCalculator.SquareRoot(-4)); // sqrt(-4) → -1
+        }
+
+        [Test]
+        public void Log_ValidAndInvalidInputs()
+        {
+            Assert.AreEqual(0, ScientificCalculator.Log(1));  // log(1) = 0
+            Assert.AreEqual(0, ScientificCalculator.Log(1));  // log(1) = 0
+            Assert.AreEqual(1, ScientificCalculator.Log(Math.E), 0.0001);  // log(e) = 1
+            Assert.AreEqual(Math.Log(10), ScientificCalculator.Log(10), 0.0001); // log(10) ≈ 2.3
+            Assert.AreEqual(-1, ScientificCalculator.Log(0));  // log(0) → -1
+            Assert.AreEqual(-1, ScientificCalculator.Log(-5)); // log(-5) → -1
+        }
+    }
+}
