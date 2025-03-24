e33013b169726023f15fd8e95f4af3bcacff88b5
Piotrus00 <117126497+Piotrus00@users.noreply.github.com>
2025-03-24 22:56:59 +0100

    Add files via upload

---
+++ Kalkulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kalkulator;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Kalkulator!");
+        CalculatorService.Run();
+    }
+}
